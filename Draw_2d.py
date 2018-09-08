32cad5aa5a0e07cdb71fb59a08278b9848f90b0d
Zhao <tony.zhao2@thermofisher.com>
2018-09-07 17:31:38 -0700

    a working heatmap kind plot

--- Draw_2d.py
+++ Draw_2d.py
@@ -3,6 +3,8 @@ import pandas as pd
 import filePatch
 import numpy as np
 import matplotlib.pylab as plt
+import scipy
+from scipy.interpolate import interp2d
 from mpl_toolkits.mplot3d import Axes3D
 
 
@@ -19,7 +21,7 @@ class Draw_2d:
         pts = list(map(lambda x, y: x + tuple([y]), data.index.values.tolist(), np.squeeze(data.values)))
         plt.style.use('seaborn-white')
         ax = plt.figure().gca(projection='3d')
-        skip_every = 50
+        skip_every = 5
         xs, ys, zs = list(zip(*pts))[0][::skip_every], list(zip(*pts))[1][::skip_every], list(zip(*pts))[2][::skip_every]
         ax.scatter(xs, ys, zs)
         ax.plot(xs, ys, zs)
@@ -28,47 +30,61 @@ class Draw_2d:
         ax.set_zlabel('Signal Intensity')
         plt.show()
 
-    def draw2dheat(self):
+    def draw2dheatmap(self):
         data = self.data
         pts = list(map(lambda x, y: x + tuple([y]), data.index.values.tolist(), np.squeeze(data.values)))
-        plt.style.use('seaborn-white')
-        skip_every = 10
+        skip_every = 5
         xs, ys, zs = list(zip(*pts))[0][::skip_every], list(zip(*pts))[1][::skip_every], list(zip(*pts))[2][
                                                                                          ::skip_every]
-        # https://stackoverflow.com/questions/26635870/plot-x-y-z-triples-on-2d-plane-with-a-colormap
-        xx, yy = np.meshgrid[0:max(ys):800j, 0:max(xs):800j]
-        # zz =
-        plt.imshow()
+        f = interp2d(xs, ys, zs, kind="linear")
+
+        x_coords = np.arange(min(xs), max(xs) + 1)
+        y_coords = np.arange(min(ys), max(ys) + 1)
+        Z = f(x_coords, y_coords)
+
+        fig = plt.imshow(Z,
+                         extent=[min(xs), max(xs), min(ys), max(ys)],
+                         origin="lower", aspect='auto')
+
+        fig.axes.set_autoscale_on(False)
+        plt.scatter(xs, ys, 400, facecolors='none')
+
         plt.xlabel('Collision Energy')
         plt.ylabel('Q2RF')
         plt.show()
 
-    def draw3d_mayavi(self):
+    def draw2d_scipy(self):
         '''
-        Half way down. Guess because parts of data missing
+        The best plot solution.
+        Interestingly a simple: plt.scatter(xs, ys, c=zs) works as well.
         :return:
         '''
-        from mayavi import mlab
         data = self.data
         pts = list(map(lambda x, y: x + tuple([y]), data.index.values.tolist(), np.squeeze(data.values)))
-        plt.style.use('seaborn-white')
-        ax = plt.figure().gca(projection='3d')
-        skip_every = 10
+        skip_every = 5
         xs, ys, zs = list(zip(*pts))[0][::skip_every], list(zip(*pts))[1][::skip_every], list(zip(*pts))[2][
                                                                                          ::skip_every]
 
-        pts = mlab.points3d(xs, ys, zs, zs)
-        mesh = mlab.pipeline.delaunay2d(pts)
-        pts.remove()
+        plt.scatter(xs, ys, c=zs)
+        # Set up a regular grid of interpolation points
+        xi, yi = np.linspace(min(xs), max(xs), 100), np.linspace(min(ys), max(ys), 100)
+        xi, yi = np.meshgrid(xi, yi)
 
-        surf = mlab.pipeline.surface(mesh)
+        # Interpolate
+        rbf = scipy.interpolate.Rbf(xs, ys, zs, function='linear')
+        zi = rbf(xi, yi)
 
-        mlab.xlabel('Collision Energy')
-        mlab.ylabel('Q2RF')
-        mlab.show()
+        plt.imshow(zi, vmin=min(zs), vmax=max(zs), origin='lower',
+                   extent=[min(xs), max(xs), min(ys), max(ys)], aspect='auto')
+        #plt.scatter(xs, ys, c=zs)
+        plt.colorbar()
+        plt.show()
 
+    
 
 if __name__ == '__main__':
     draw_2d = Draw_2d()
-    draw_2d.get_dataframe('322-60')
-    draw_2d.draw3d()
+    draw_2d.get_dataframe('468-121_n')
+    #draw_2d.draw2dheatmap()
+    #draw_2d.draw3d()
+    draw_2d.draw2d_scipy()
